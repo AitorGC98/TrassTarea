bb2c1795c1b0e3d36d7b88a482af7d0ca5a9307b
Aitor <garciacuradoaitor@gmail.com>
2023-11-05 19:36:17 +0100

    verion 0.2
    Añadido cambio de actividades

--- app/src/main/java/com/example/trasstarea/MainActivity.java
+++ app/src/main/java/com/example/trasstarea/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.trasstarea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnEmpezar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
+
+        btnEmpezar=findViewById(R.id.btn_empezar);
+        btnEmpezar.setOnClickListener(this::enviar);
+
+
+    }
+    public void enviar(View view) {
+        Intent intent = new Intent(this, ListadoTareasActivity.class);
+        startActivity(intent);
     }
 }
